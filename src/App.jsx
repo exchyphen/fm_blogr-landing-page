9b91a045d736f6192e0bee0aa51c85fe95e391dc
exchyphen <exc.hyphen@gmail.com>
2024-02-27 20:34:47 -0800

    added sections 1 and 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import "./App.css";
 import Nav__Logo from "./assets/images/logo.svg";
 import IconArrowDesktop from "./assets/images/icon-arrow-light.svg";
 import IconArrowMobile from "./assets/images/icon-arrow-dark.svg";
-import PatternCircles from "./assets/images/bg-pattern-circles.svg";
-import EditorDesktop from "./assets/images/illustration-editor-desktop.svg";
 import Phones from "./assets/images/illustration-phones.svg";
 
 function App() {
@@ -208,12 +206,49 @@ function App() {
       </header>
       <main>
         <section className="feature1">
-          <h1>Designed for the future</h1>
-          <div className="test">
-            <img src={Phones}></img>
-            <img src={EditorDesktop}></img>
+          <h1 className="feature1__h1">Designed for the future</h1>
+          <div className="feature1__editor-container">
+            <div className="feature1__editor-item">
+              <h2>Introducing an extensible editor</h2>
+              <p>
+                Blogr features an exceedingly intuitive interface which lets you
+                focus on one thing: creating content. The editor supports
+                management of multiple blogs and allows easy manipulation of
+                embeds such as images, videos, and Markdown. Extensibility with
+                plugins and themes provide easy ways to add functionality or
+                change the looks of a blog.
+              </p>
+            </div>
+            <div className="feature1__editor-item">
+              <h2>Robust content management</h2>
+              <p>
+                Flexible content management enables users to easily move through
+                posts. Increase the usability of your blog by adding customized
+                categories, sections, format, or flow. With this functionality,
+                you’re in full control.
+              </p>
+            </div>
           </div>
         </section>
+        <section className="feature2">
+          <img
+            className="feature2__phones-img"
+            src={Phones}
+            alt="phones img"
+          ></img>
+          <div className="feature2__container">
+            <h3 className="feature2__header">
+              State of the Art Infrastructure
+            </h3>
+            <p className="feature2__text">
+              With reliability and speed in mind, worldwide data centers provide
+              the backbone for ultra-fast connectivity. This ensures your site
+              will load instantly, no matter where your readers are, keeping
+              your site competitive.
+            </p>
+          </div>
+        </section>
+        <section className="feature3"></section>
       </main>
     </>
   );
